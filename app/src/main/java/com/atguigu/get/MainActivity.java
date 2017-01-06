1f4c66c8fd9ad5f48d6963ef0691e39a5d0359f6
ITLC <1740104885@qq.com>
2017-01-06 12:05:05 +0800

    3

--- app/src/main/java/com/atguigu/get/MainActivity.java
+++ app/src/main/java/com/atguigu/get/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toast.makeText(MainActivity.this, "111111", Toast.LENGTH_SHORT).show();
         Toast.makeText(MainActivity.this, "2", Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainActivity.this, "3", Toast.LENGTH_SHORT).show();
     }
 }
